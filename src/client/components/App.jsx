016237f29af0ea29a5263b4fbc6fa3ccf750e1e0
Dick van Hirtum <dvhirtum@gmail.com>
2018-05-21 14:01:46 +0200

    Initial express and react setup

---
+++ src/client/components/App.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return <h1>Content goes here...</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, './build');
+const APP_DIR = path.resolve(__dirname, './src/client');
+
+const config = {
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  entry: {
+    main: `${APP_DIR}/index.jsx`
+  },
+  output: {
+    filename: 'bundle.js',
+    path: BUILD_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.css|.scss)$/,
+        use: [{
+          loader: 'style-loader' // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader' // translates CSS into CommonJS
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(jsx|js)?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+            presets: ['react', 'es2015'] // Transpiles JSX and ES6
+          }
+        }]
+      }
+    ],
+  }
+};
+
+module.exports = config;
