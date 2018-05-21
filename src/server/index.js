f5f69550e155e5ce2158109b7c4584b8eaf234e5
Dick van Hirtum <dvhirtum@gmail.com>
2018-05-21 14:17:51 +0200

    move static file to server folder

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const debug = require('debug')('server');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', '..')));
+const port = 3000;
 
-app.listen(3000);
+app.use(morgan('tiny'));
+app.use('/build', express.static(path.join(__dirname, '..', '..', 'build')));
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => {
+  debug(`listening on port ${port}`);
+});
