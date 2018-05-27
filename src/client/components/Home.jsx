aacab71d5271f43091a139f8eec9c63d6a7ed22e
Dick van Hirtum <dvhirtum@gmail.com>
2018-05-27 19:20:31 +0200

    Add some instructions

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Container, Button } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { history } from '../store/configureStore';
 
 const Home = () => (
   <Container>
-    <Route render={({ history }) => (
-      <Button onClick={() => history.push('/instructions')}>
-        Instructions
-      </Button>)}
-    />
-    <Route render={({ history }) => (
-      <Button primary onClick={() => history.push('/round/1')}>
-        Start the game
-      </Button>)}
-    />
+    <Button onClick={() => history.push('/instructions')}>Instructions</Button>
+    <Button primary onClick={() => history.push('/round/1')}>Start the game</Button>
   </Container>
 );
 
--- src/client/components/Instructions.jsx
+++ src/client/components/Instructions.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Container, Header, Button } from 'semantic-ui-react';
-import { Route } from 'react-router-dom';
+import { history } from '../store/configureStore';
 
 const Instructions = () => (
   <Container>
     <Header as="h2">Instructions</Header>
-    <Route render={({ history }) => (
-      <Button onClick={() => history.push('/')}>
-        Back
-      </Button>)}
-    />
+    <p>You will be presented with 9 random movies from your Plex collection, in 3 rounds of 3 movies each.</p>
+    <p>In each round, select the movie that you would like to see the most.</p>
+    <p>In the final round, you will be presented with the winning movies from the first 3 rounds.</p>
+    <p>Again, select the movie that you would like to see the most. That is the winning movie.</p>
+    <Button onClick={() => history.push('/')}>Back</Button>
   </Container>
 );
 
